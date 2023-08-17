388bfa862f9c656e002173ada8d122580e6ec8cd
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2023-08-17 22:31:53 +0300

    Fix log grouping
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- apps/text-inference-batcher-nodejs/src/waitOrThrow.ts
+++ apps/text-inference-batcher-nodejs/src/waitOrThrow.ts
@@ -7,18 +7,19 @@ export async function waitOrThrow (model: string, MAX_CONNECT_PER_UPSTREAM?: str
   const timeout = parseInt(TIMEOUT ?? "600000");
   const waitFor = parseInt(WAIT_FOR ?? "5000");
   let waiting = 0;
+  console.group(`wait or thorow model: ${model}`);
   // keep waiting if there is no free (connections < MAX_CONNECT_PER_UPSTREAM) upstream with the matching model
   while (
     state.filterByModel(model).filter(({ connections }) => connections < maxConnection).length === 0
   ) {
-    console.group(`wait or thorow model: ${model}`);
     console.info(`waiting ${waitFor}ms for a free upstream with model ${model}...`);
     await new Promise((resolve) => setTimeout(resolve, waitFor));
     waiting += 1000;
     if (waiting >= timeout) {
       console.error(`timeout (${timeout}) waiting for a free upstream`);
-      console.group(`wait or thorow model: ${model}`);
+      console.groupEnd();
       throw new HTTPException(503, { message: "Timeout waiting for a free upstream, try again later" });
     }
   }
+  console.groupEnd();
 }
