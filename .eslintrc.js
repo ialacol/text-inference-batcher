d9a2bd7bfa971e3ca5142a20b339bef375065f19
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2023-08-24 22:55:30 +0300

    Turn on isolatedModules/noUncheckedIndexedAccess
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,18 @@
 module.exports = {
   ignorePatterns: ["**/dist/*.*"],
-  extends: [
-    "standard",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier"
-  ],
+  extends: ["standard", "eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module"
+    sourceType: "module",
   },
-  plugins: [
-    "@typescript-eslint"
-  ],
+  plugins: ["@typescript-eslint"],
   rules: {
-    indent: [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    quotes: [
-      "error",
-      "double"
-    ],
-    semi: [
-      "error",
-      "always"
-    ]
-  }
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    // fight with tsconfig's noUncheckedIndexedAccess
+    "dot-notation": 0,
+  },
 };
--- apps/text-inference-batcher-nodejs/src/index.ts
+++ apps/text-inference-batcher-nodejs/src/index.ts
@@ -7,7 +7,7 @@ import { updateUpstreamState } from "./updateUpstreamState.js";
 
 app.use("*", logger(), cors());
 
-const port = parseInt(process.env.TIB_PORT ?? "8000");
+const port = parseInt(process.env["TIB_PORT"] ?? "8000");
 serve({
   fetch: app.fetch,
   port,
@@ -15,16 +15,16 @@ serve({
 
 console.info(`\ntib is listening on http://localhost:${port}`);
 console.table({
-  UPSTREAMS: process.env.UPSTREAMS,
-  MAX_CONNECT_PER_UPSTREAM: process.env.MAX_CONNECT_PER_UPSTREAM,
-  WAIT_FOR: process.env.WAIT_FOR,
-  TIMEOUT: process.env.TIMEOUT,
-  DEBUG: process.env.DEBUG,
-  TIB_PORT: process.env.TIB_PORT,
+  UPSTREAMS: process.env["UPSTREAMS"],
+  MAX_CONNECT_PER_UPSTREAM: process.env["MAX_CONNECT_PER_UPSTREAM"],
+  WAIT_FOR: process.env["WAIT_FOR"],
+  TIMEOUT: process.env["TIMEOUT"],
+  DEBUG: process.env["DEBUG"],
+  TIB_PORT: process.env["TIB_PORT"],
 });
 
-if (process.env.UPSTREAMS) {
-  const urls = parseUpstreams(process.env.UPSTREAMS);
+if (process.env["UPSTREAMS"]) {
+  const urls = parseUpstreams(process.env["UPSTREAMS"]);
   updateUpstreamState(urls).catch((error) => {
     console.error(error);
   });
@@ -44,6 +44,6 @@ const shutdown = (signal: string, value: number) => {
 Object.keys(signals).forEach((signal) => {
   process.on(signal, () => {
     console.warn(`process received a ${signal} signal`);
-    shutdown(signal, signals[signal]);
+    shutdown(signal, signals[signal] ?? 15);
   });
 });
